d9da8dec45527a2c0a05638e9a891df2a9f9594a
Genie <gspaul6@gmail.com>
2019-04-30 14:09:40 +0200

    Composant Collegue - Modifier

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -8,18 +8,25 @@ import { Collegue } from '../models/Collegue';
   styleUrls: ['./collegue.component.css']
 })
 export class CollegueComponent implements OnInit {
-  
-  
-  
+  modifier:boolean=true;
+  newCollegueClick:boolean=true;
+   nameModifier:string="Modifier";
     @Input()col:Collegue;
   
     @Output() change:EventEmitter<string> = new EventEmitter<string>();
 
   newCollegue(){
     this.change.emit('du nouveau utiliser')
+    //this.newCollegueClick=true;
   }
   Modifier(){
     this.change.emit('Modifier')
+    //this.modifier=true;
+  if(this.modifier){
+    this.nameModifier="Valider";
+  }else{
+    this.nameModifier="Modifier";
+  }
   }
 
   ngOnInit() {
