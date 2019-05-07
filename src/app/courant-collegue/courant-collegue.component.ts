93a39ae8ebe8c072119bc7ec08285aff76223e7d
Genie <gspaul6@gmail.com>
2019-05-07 15:33:47 +0200

    photos with opening of new page

--- src/app/courant-collegue/courant-collegue.component.ts
+++ src/app/courant-collegue/courant-collegue.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Collegue } from '../models/Collegue';
+import { ActivatedRoute, ParamMap,  } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-courant-collegue',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courant-collegue.component.css']
 })
 export class CourantCollegueComponent implements OnInit {
-
-  constructor() { }
+CollegueClicked:Collegue;
+matricule:string;
+service:DataService;
+message:string;
+  constructor(private route: ActivatedRoute, service:DataService) { 
+this.service=service;
+  }
 
   ngOnInit() {
+ this.matricule=this. route.snapshot.paramMap.get('matricule');
+ this.service.recupererCollegueCourant(this.matricule).subscribe(collegue => {
+   this.CollegueClicked=collegue;
+ }, err=> {
+  this.message=`${err.error}`;
+});
   }
 
 }
